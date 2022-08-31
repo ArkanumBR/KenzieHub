2b431d84640e093db353f943237f7a095d2e5593
Paulo Gustavo <arknum@hotmail.com>
2022-08-31 16:16:10 -0300

    correcao erros

--- src/contexts/register.jsx
+++ src/contexts/register.jsx
@@ -1,22 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import axios from 'axios';
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
 
 
-
+ 
 export const RegContext = createContext({});
 
 const RegisterProvider = ({ children }) => {
- 
+  const navigate = useNavigate(); 
 
   const schema = yup.object({
       name: yup.string().required("Campo necessário."),
       email: yup.string().email("Deve ser um e-mail válido").required("Campo necessário."),
       password: yup.string().matches(
+          // eslint-disable-next-line no-useless-escape
           /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
           "Necessário 8 caracteres, uma letra maiúscula, uma letra minúscula, um numero e um caracter especial"
         ).required("Campo necessário."),
@@ -25,8 +26,8 @@ const RegisterProvider = ({ children }) => {
       contact: yup.string()
       .required("Campo necessário.")
       .matches(/^[0-9]+$/, "Somente números")
-      .min(10, 'Precisa ser um telefone válido')
-      .max(11, 'Precisa ser um telefone válido'),
+      .min(10, 'Precisa ser um telefone válido no formato, como por exemplo: 38998976570')
+      .max(11, 'Precisa ser um telefone válido no formato, como por exemplo: 38998976570'),
       course_module: yup.string().required("Campo necessário."),
 
   });
@@ -36,15 +37,16 @@ const RegisterProvider = ({ children }) => {
   });
   
   
- function registerUser(data){
-  console.log(data);
-
-  axios.post("https://kenziehub.herokuapp.com/users", data)
- .then((response) => console.log(response.data))
- .catch((error) => console.log(error));
-
- toast.success("Cadastro feito com sucesso!")
- 
+ async function registerUser(data){
+  await axios.post("https://kenziehub.herokuapp.com/users", data)
+ .then((response) => {
+  console.log(response.data)
+  toast.success("Cadastro feito com sucesso!")
+  navigate("/")
+})
+ .catch((error) => {
+  toast.error("Email já cadastrado!")
+  console.log(error)}); 
  }
 
    
